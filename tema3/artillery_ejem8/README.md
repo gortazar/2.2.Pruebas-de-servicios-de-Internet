c60fecc284d6093921839e536c4d73cc785ddc87
Maes95 <michel.maes95@gmail.com>
2020-01-09 02:18:36 +0100

    Add new examples of Artillery

---
+++ tema3/artillery_ejem8/README.md
@@ -0,0 +1,20 @@
+# SpringBoot-Tomcat-WebChat
+
+This is a distributed WebChat with SpringBoot-Tomcat.
+
+# Prerequisites
+
+* Java 8
+* Maven 3.0.5
+
+## SetUp
+
+```sh
+$ mvn install
+```
+
+## Start application
+
+```sh
+$ mvn spring-boot:run
+```
---
+++ tema3/artillery_ejem8/src/main/java/com/example/chat/WebChatSpringBootApplication.java
@@ -0,0 +1,31 @@
+package com.example.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableWebSocket
+public class WebChatSpringBootApplication extends SpringBootServletInitializer{
+
+	@Bean
+	public ChatManager reverseWebSocketEndpoint() {
+		return new ChatManager();
+	}
+
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatSpringBootApplication.class, args);
+	}
+
+}
+
