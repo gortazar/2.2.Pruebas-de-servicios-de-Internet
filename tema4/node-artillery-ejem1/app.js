b139f77853c7e59c166a507aae0a967a6fb20ba0
Maes95 <michel.maes95@gmail.com>
2020-01-09 16:44:24 +0100

    Add WebSocket-Node example of Artillery

---
+++ tema4/node-artillery-ejem1/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const SocketServer = require('ws').Server;
+const http = require('http');
+const port = 5000;
+const server = http.createServer(app).listen(port, ()=>{
+  console.log("Running app in localhost:"+port)
+});
+const path = require('path')
+
+// To use client files
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+DUPLICATE_MSG = "{\"type\":\"system\",\"message\":\"User already exist\"}";
+
+const wss = new SocketServer({ server });
+
+// Set where key is the user_name and the value is the chat_name
+var users = new Set();
+
+wss.on('connection', (ws) => {
+
+	ws.on('message', (data) => {
+		var message = JSON.parse(data);
+		if(message['message']){
+			// Normal message (broadcast it)
+			wss.clients
+      .forEach( (client) => {if(client.chat == ws.chat) client.send(data)} );
+		}else{
+			// First message (first connection)
+      if(users.has(message['name'])){
+        ws.send(DUPLICATE_MSG);
+        ws.close();
+      }else{
+        users.add(message['name']);
+        ws['chat'] = message['chat'];
+        ws['name'] = message['name'];
+      }
+		}
+	});
+
+  ws.on('close', () => {
+    users.delete(ws['name']);
+  });
+
+});
